ed1c9b8509149130892de72a4360e370b2e060c0
Stefan Ravn van Overeem <stefan@stefanrvo.dk>
2017-06-20 20:15:58 +0200

    Increased number of filedescriptors.

--- software/main/FilesystemHandler.cpp
+++ software/main/FilesystemHandler.cpp
@@ -14,7 +14,7 @@ void FilesystemHandler::register_filesystem(const char *partition_name, const ch
 
     esp_vfs_fat_mount_config_t mount_config;
     mount_config.format_if_mount_failed = false;
-    mount_config.max_files = 4;
+    mount_config.max_files = 20;
     esp_err_t err = esp_vfs_fat_spiflash_mount(mountpt, partition_name, &mount_config, &s_wl_handle);
     if (err != ESP_OK) {
         ESP_LOGE(TAG, "Failed to mount FATFS (0x%x)", err);
